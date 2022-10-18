84e58236540668893020f6ec8403ae5de9eb1692
IB-315168 <315168@via.dk>
2022-10-18 13:33:51 +0200

    UserHttpClient Create Async implementation

--- HttpClients/Implementations/UserHttpClient.cs
+++ HttpClients/Implementations/UserHttpClient.cs
@@ -1,18 +1,40 @@
-﻿using System;
+﻿using Domain.DTOs;
+using Domain.Models;
+using HttpClients.ClientInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace HttpClients.Implementations
 {
-    public class UserHttpClient
+    public class UserHttpClient : IUserService
     {
         private readonly HttpClient client;
 
         public UserHttpClient(HttpClient client)
         {
             this.client = client;
+        }
+
+        public async Task<User> CreateAsync(UserCreationDTO dto)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("/users", dto);
+            string result = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(result);
+            }
+
+            User user = JsonSerializer.Deserialize<User>(result, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            })!;
+
+            return user;
         }
     }
 }
