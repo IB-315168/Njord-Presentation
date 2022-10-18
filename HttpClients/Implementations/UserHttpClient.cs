a937c701133e33efa336f1df7c45e61fbbce9af0
IB-315168 <315168@via.dk>
2022-10-18 13:30:55 +0200

    Blazor set-up

---
+++ HttpClients/Implementations/UserHttpClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClients.Implementations
+{
+    public class UserHttpClient
+    {
+        private readonly HttpClient client;
+
+        public UserHttpClient(HttpClient client)
+        {
+            this.client = client;
+        }
+    }
+}
