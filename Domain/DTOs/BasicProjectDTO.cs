7edd342daf3efda9f021af9d1d85e41affd8f864
IB-315168 <315168@via.dk>
2022-11-21 23:49:51 +0100

    My Projects implementation + handling

--- Domain/DTOs/BasicProjectDTO.cs
+++ Domain/DTOs/BasicProjectDTO.cs
@@ -2,14 +2,14 @@
 
 public class BasicProjectDTO
 {
-    private int id { get; }
-    private string projectName { get; }
-    private string teamName { get; }
+    public int Id { get; }
+    public string ProjectName { get; }
+    public string TeamName { get; }
 
     public BasicProjectDTO(int id, string projectName, string teamName)
     {
-        this.id = id;
-        this.projectName = projectName;
-        this.teamName = teamName;
+        this.Id = id;
+        this.ProjectName = projectName;
+        this.TeamName = teamName;
     }
 }
