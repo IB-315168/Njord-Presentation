cda4f9a304186b277823d3da61a1ecb65261d6c8
IB-315168 <315168@via.dk>
2022-11-22 12:01:41 +0100

    Initial - Radzen Import + ViewProject

--- Njord/Program.cs
+++ Njord/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Njord;
+using Radzen;
 using Domain.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -12,6 +13,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7033") });
+
+builder.Services.AddScoped<DialogService>();
 
 builder.Services.AddScoped<IUserService, UserHttpClient>();
 builder.Services.AddScoped<ITeamService, TeamHttpClient>();
