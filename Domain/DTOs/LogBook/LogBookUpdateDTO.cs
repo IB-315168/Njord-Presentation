7dff6f0d8defffc0e814ecf223deb16c2610b0a9
Dércio Fernandes <315236@via.dk>
2022-12-03 13:52:17 +0100

    LogBookHttpClient + LogBookEntity and DTO´s have been added.

---
+++ Domain/DTOs/LogBook/LogBookUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+
+namespace Domain.DTOs.LogBook;
+
+public class LogBookUpdateDTO
+{
+    public int Id { get; }
+    public ICollection<LogBookEntryEntity> entries { get; set; }
+
+    public LogBookUpdateDTO(int id)
+    {
+        Id = id;
+    }
+    
+    
+}
---
+++ HttpClients/ClientInterfaces/ILogBookService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.DTOs.LogBook;
+using Domain.DTOs.Project;
+using Domain.Models;
+
+namespace HttpClients.ClientInterfaces;
+
+public interface ILogBookService
+{
+    Task<LogBookEntity> CreateAsync(LogBookCreateDTO dto);
+    Task UpdateAsync(LogBookUpdateDTO dto);
+    Task<LogBookEntity> GetByIdAsync(int id);
+}
