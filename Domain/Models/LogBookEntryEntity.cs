4d597a38d8a95cf800b17cb75a22f26ef9057867
IB-315168 <315168@via.dk>
2022-12-04 12:32:56 +0100

    LogBookEntity alter

--- Domain/Models/LogBookEntryEntity.cs
+++ Domain/Models/LogBookEntryEntity.cs
@@ -4,10 +4,10 @@ public class LogBookEntryEntity : IEquatable<LogBookEntryEntity>
 {
     public int Id { get; set; }
     public int IdLogBook { get; set; }
-    public int IdMeeting { get; set; }
+    public MeetingEntity IdMeeting { get; set; }
     public string content { get; set; }
 
-    public LogBookEntryEntity(int id, int idLogBook, int idMeeting, string content)
+    public LogBookEntryEntity(int id, int idLogBook, MeetingEntity idMeeting, string content)
     {
         Id = id;
         IdLogBook = idLogBook;
@@ -22,7 +22,7 @@ public class LogBookEntryEntity : IEquatable<LogBookEntryEntity>
             return false;
         }
 
-        return Id == other.Id && IdLogBook == other.IdLogBook && IdMeeting == other.IdLogBook &&
+        return Id == other.Id && IdLogBook == other.IdLogBook &&
                content.Equals(other.content);
     }
 }
